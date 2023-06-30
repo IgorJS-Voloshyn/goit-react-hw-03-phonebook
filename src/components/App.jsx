78fc3e93a1ae3e79ed3794021c6c631e3e6e5c88
IgorJS-Voloshyn <igorVoloshyn.js@gmail.com>
2023-06-30 20:46:28 +0300

    fixing filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,13 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   componentDidMount() {
@@ -59,15 +65,28 @@ export class App extends Component {
     });
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  showContact = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   render() {
+    const showContacts = this.showContact();
     return (
       <div className={css.container}>
         <h1 className={css.title}>Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHundler} />
         <h2 className={css.title}>Constacts</h2>
-        <Filter contacts={this.state.contacts} />
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
         <ContactList
-          contacts={this.state.contacts}
+          contacts={showContacts}
           del={this.deleteContactFromContactList}
         />
       </div>
